a3c6516bf0a669c6027f1afd201376e809ada17f
Tyler Goodlet <tgoodlet@gmail.com>
2012-10-11 00:45:06 -0400

    update to splitwin.sh

--- splitwin.sh
+++ splitwin.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # vertically split the tmux console and run the passed command in lower pane
+# this script is mostly used for help docs called from vim
+args="$*"
+
 if [[ $# < 1 ]]; then
     echo "$0: FAILURE you did not pass a command to run?"
     echo "most likely opened a 'blank' tmux pane..."
+elif [[ $args  =~ hoogle ]]; then
+    args="$args |less"
 fi
 
-exec tmux split-window -v "$*"
+tmux split-window -v "$args"
