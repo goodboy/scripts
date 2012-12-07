98b71fb9330b375830b1b499f1c65c81e2789048
Tyler Goodlet <tgoodlet@gmail.com>
2012-12-06 21:44:08 -0500

    splitwin: add extra stuff that is not used yet

--- splitwin.sh
+++ splitwin.sh
@@ -2,12 +2,32 @@
 # vertically split the tmux console and run the passed command in lower pane
 # this script is mostly used for help docs called from vim
 args="$*"
+declare -i lines=1
+# shopt -s checkwinsize
 
 if [[ $# < 1 ]]; then
+
     echo "$0: FAILURE you did not pass a command to run?"
     echo "most likely opened a 'blank' tmux pane..."
-elif [[ $args  =~ hoogle ]]; then
-    args="$args |less"
-fi
 
-tmux split-window -v "$args"
+elif [[ $args  =~ hoogle ]]; then
+
+    # output="$($args)"
+    # while read line; do
+    #     if [[ $line =~ "\n" ]]; then
+    #         lines=+1
+    #     fi
+    #     if (( $lines > 26 )); then
+    #         lines= 26
+    #         break
+    #     fi
+    # done <<< $output
+    args="$args |less"
+
+    # echo "output = $output"
+    # echo "and is $lines lines long"
+
+    # tmux split-window -l $lines -v "less <<< $output"
+# fi
+    tmux split-window -l 15 -v "$args"
+fi
