8b674b0070995d6c62ccd2f654dc0da739e8d495
Tyler Goodlet <tgoodlet@gmail.com>
2019-04-24 00:10:42 -0400

    Adjust screen orientation, don't use xmodmap

--- xps_screen_fix.sh
+++ xps_screen_fix.sh
@@ -1,23 +1,25 @@
 #!/bin/sh
+# set -x
 
 keyboard_map () {
     # apply keyboard re-maps
-    xmodmap ~/.Xmodmap
+    setxkbmap -option caps:ctrl_modifier
 }
 
+
+# get display names
+EXT=`xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep -v ^eDP | head -n 1`
+echo "EXT is '$EXT'"
+INT=`xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep -v ^DP | head -n 1`
+echo "INT is '$INT'"
 
 function extend() {
     # extend non-HiDPI external display on DP* above HiDPI internal display eDP*
     # see also https://wiki.archlinux.org/index.php/HiDPI
     # you may run into https://bugs.freedesktop.org/show_bug.cgi?id=39949
     #                  https://bugs.launchpad.net/ubuntu/+source/xorg-server/+bug/883319
-    set -x
 
-    EXT=`xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep -v ^eDP | head -n 1`
-    echo "EXT is '$EXT'"
-    INT=`xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep -v ^DP | head -n 1`
-    echo "INT is '$INT'"
-
+    # get dimensions
     ext_w=`xrandr | sed 's/^'"${EXT}"' [^0-9]* \([0-9]\+\)x.*$/\1/p;d'`
     echo "ext_w is '$ext_w'"
     ext_h=`xrandr | sed 's/^'"${EXT}"' [^0-9]* [0-9]\+x\([0-9]\+\).*$/\1/p;d'`
@@ -27,12 +29,20 @@ function extend() {
     off_w="$(echo $(( ($int_w-$ext_w) / 2 )) | sed 's/^-//')"
     echo "off_w is '$off_w'"
 
-    xrandr --output "${INT}" --auto --pos ${off_w}x${ext_h} --scale 1x1  --output "${EXT}" --auto --scale 2.3x2.3 --pos 0x0
+    # xrandr --output "${INT}" --auto --pos ${off_w}x${ext_h} --scale 1x1  --output "${EXT}" --auto --scale 2.3x2.3 --pos 0x0
+    # xrandr --output "${INT}" --pos ${off_w}x${ext_h} --auto --scale 1x1  --output "${EXT}" --right-of $INT --pos 0x0
+    xrandr --output "${INT}" --pos ${off_w}x${ext_h} --auto --scale 1x1  --output "${EXT}" --right-of $INT --pos 0x0
 }
 
 
+function display_off() {
+    xrandr --output eDP-1 --auto --output $EXT --off
+}
+
 keyboard_map
-xrandr --output eDP1 --auto --output HDMI1 --auto
-# TODO: figure out why this needs to be called twice...
-extend
+
+ # xrandr --output eDP-1 --auto --output HDMI-1 --auto
+
+xrandr --output $INT --right-of $EXT --output $EXT --auto
+
 extend
